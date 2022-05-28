3af8f9969147ee4db6f348a567dcd44c374047e0
shashikant maurya <shashikantmaurya0007@gmail.com>
2022-05-28 13:20:54 +0530

    feat:tailwind.config file update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,tsx,ts}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightPrimary: "#5458ff",
+        lightSecondary: "#3f44ff",
+        lightBackground: "#CEDEF3",
+        lightHover: "#8CB2E3",
+        darkPrimary: "#3864BC",
+        darkSecondary: "#3864BC",
+        darkBackground: `#3864BC`,
+        darkHover: "#8CB2E3",
+      },
+    },
   },
   plugins: [],
 };
