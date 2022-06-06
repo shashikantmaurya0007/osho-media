a212d69bbf7c0c3018abab824db51be3330b3b0c
shashikant maurya <shashikantmaurya0007@gmail.com>
2022-06-06 12:56:36 +0530

    refactor:removed console

--- src/store/InitialAppLoadingAction/initialLoginAction.js
+++ src/store/InitialAppLoadingAction/initialLoginAction.js
@@ -4,8 +4,7 @@ const initalLoginAction = () => {
   return async (dispatch) => {
     const encodedToken = localStorage.getItem("encodedToken");
     const userInformation = JSON.parse(localStorage.getItem("userInformation"));
-    console.log(encodedToken !== null);
-    console.log(encodedToken, "efsfsdf");
+
     if (encodedToken) {
       dispatch(loginActions.setEncodedToken(encodedToken));
       dispatch(loginActions.setUserInformation(userInformation));
