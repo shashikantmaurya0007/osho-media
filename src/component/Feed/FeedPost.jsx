1d2089a670899c18720d72e3557073854e871300
shashikant maurya <shashikantmaurya0007@gmail.com>
2022-06-12 21:34:17 +0530

    feat:-followed user post in feed page

--- src/component/Feed/FeedPost.jsx
+++ src/component/Feed/FeedPost.jsx
@@ -4,15 +4,22 @@ import { PostCard } from "../PostCard/PostCard.jsx";
 
 const FeedPost = () => {
   const allPosts = useSelector((state) => state.post.allPost);
-  const { username } = useSelector((state) => state.login.userInformation);
-  const feedPosts = allPosts?.filter((post) => post?.username === username);
+  const { username, following } = useSelector(
+    (state) => state.login.userInformation
+  );
+  const followedByuser = following.map((ele) => ele.username);
+  debugger;
+  const feedPosts = allPosts?.filter(
+    (post) =>
+      post?.username === username || followedByuser.includes(post?.username)
+  );
 
   return (
     <div className="flex flex-col gap-6 my-6">
       {feedPosts?.map((ele) => (
         <PostCard key={ele?._id} postdata={ele} />
       ))}
-  </div>
+    </div>
   );
 };
 
